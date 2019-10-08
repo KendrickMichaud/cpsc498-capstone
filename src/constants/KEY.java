1e8d2fc58632847c991ec4d7ae86940207dc4ccb
Kendrick Michaud <kendrick.michaud.17@cnu.edu>
2019-10-08 15:31:10 -0400

    Refactored Keys to allow for high keys and low keys.

--- src/constants/KEY.java
+++ src/constants/KEY.java
@@ -1,49 +1,102 @@
 package constants;
 
+//TODO: Need to refactor HighKeys to have a H_ infront of name.
+//TODO: Need to add Low Keys
+//TODO: Need to combine High and Low keys in terms of refactor
 public class KEY {
 
     //High_Keys for PlayerCharacter to Handle
-    public static final String BIOGRAPHICAL_DATA = "biographicalData*";
-    public static final String BACKGROUND = "background*";
-    public static final String PLAYER_CLASS = "playerClass*";
-    public static final String PLAYER_RACE = "playerRace*";
-    public static final String ATTRIBUTES = "attributes*";
-    public static final String INVENTORY = "inventory*";
-    public static final String SPELLBOOK = "spellbook*";
-    public static final String EQUIPPED_WEAPON = "equippedWeapon*";
-    public static final String EQUIPPED_ARMOR = "equippedArmor*";
+    public static final String H_BIOGRAPHICAL_DATA = "biographicalData*";
+    public static final String H_BACKGROUND = "background*";
+    public static final String H_PLAYER_CLASS = "playerClass*";
+    public static final String H_PLAYER_RACE = "playerRace*";
+    public static final String H_ATTRIBUTES = "attributes*";
+    public static final String H_INVENTORY = "inventory*";
+    public static final String H_SPELLBOOK = "spellbook*";
+    public static final String H_WEAPON = "equippedWeapon*";
+    public static final String H_ARMOR = "equippedArmor*";
 
+    //Low Keys
+    public static final String L_CHARACTER_NAME = "characterName";
+    public static final String L_DEITY = "deity";
+    public static final String L_WEIGHT = "weight";
+    public static final String L_ALIGNMENT = "alignment";
+    public static final String L_SIZE = "size";
+    public static final String L_HEIGHT = "height";
 
-    //Biographical Data low_keys
-    public static final String CHARACTER_NAME = BIOGRAPHICAL_DATA + "characterName";
-    public static final String DEITY = BIOGRAPHICAL_DATA + "deity";
-    public static final String ALIGNMENT = BIOGRAPHICAL_DATA +"alignment";
-    public static final String SIZE = BIOGRAPHICAL_DATA +"size";
-    public static final String HEIGHT = BIOGRAPHICAL_DATA +"height";
-    public static final String WEIGHT = BIOGRAPHICAL_DATA +"weight";
+    //Biographical Data Keys
+    public static final String K_CHARACTER_NAME = H_BIOGRAPHICAL_DATA + L_CHARACTER_NAME;
+    public static final String K_DEITY = H_BIOGRAPHICAL_DATA + L_DEITY;
+    public static final String K_ALIGNMENT = H_BIOGRAPHICAL_DATA + L_ALIGNMENT;
+    public static final String K_SIZE = H_BIOGRAPHICAL_DATA + L_SIZE;
+    public static final String K_HEIGHT = H_BIOGRAPHICAL_DATA + L_HEIGHT;
+    public static final String K_WEIGHT = H_BIOGRAPHICAL_DATA + L_WEIGHT;
+
+    public static final String L_TITLE = "title";
+    public static final String L_TRAIT = "trait";
+    public static final String L_IDEAL = "ideal";
+    public static final String L_BOND = "bond";
+    public static final String L_FLAW = "flaw";
 
     //Background low_keys
-    public static final String BACKGROUND_TITLE = BACKGROUND + "title";
-    public static final String PERSONALITY_TRAIT = BACKGROUND + "trait";
-    public static final String PERSONALITY_IDEAL = BACKGROUND + "ideal";
-    public static final String PERSONALITY_BOND = BACKGROUND + "bond";
-    public static final String PERSONALITY_FLAW = BACKGROUND + "flaw";
+    public static final String K_BACKGROUND_TITLE = H_BACKGROUND + L_TITLE;
+    public static final String K_BACKGROUND_TRAIT = H_BACKGROUND + L_TRAIT;
+    public static final String K_BACKGROUND_IDEAL = H_BACKGROUND + L_IDEAL;
+    public static final String K_BACKGROUND_BOND = H_BACKGROUND + L_BOND;
+    public static final String K_BACKGROUND_FLAW = H_BACKGROUND + L_FLAW;
     public static final String NULL = "IsNull";
 
+    public static final String L_NAME = "name";
     //PlayerRace low_keys
-    public static final String RACE_NAME = PLAYER_RACE + "name";
+    public static final String K_RACE_NAME = H_PLAYER_RACE + L_NAME;
 
     //PlayerClass low_keys
-    public static final String CLASS_NAME = PLAYER_CLASS + "name";
+    public static final String K_CLASS_NAME = H_PLAYER_CLASS + L_NAME;
+
+    public static final String L_STRENGTH = "Strength";
+    public static final String L_DEXTERITY = "Dexterity";
+    public static final String L_CONSTITUION = "Constitution";
+    public static final String L_INTELLIGENCE = "Intelligence";
+    public static final String L_WISDOM = "Wisdom";
+    public static final String L_CHARISMA = "Charisma";
 
     //Attributes low_keys
-    public static final String STRENGTH = ATTRIBUTES + "Strength";
-    public static final String DEXTERITY = ATTRIBUTES + "Dexterity";
-    public static final String CONSTITUTION = ATTRIBUTES + "Constitution";
-    public static final String INTELLIGENCE = ATTRIBUTES + "Intelligence";
-    public static final String WISDOM = ATTRIBUTES + "Wisdom";
-    public static final String CHARISMA = ATTRIBUTES + "Charisma";
+    public static final String K_STRENGTH = H_ATTRIBUTES + L_STRENGTH;
+    public static final String K_DEXTERITY = H_ATTRIBUTES + L_DEXTERITY;
+    public static final String K_CONSTITUTION = H_ATTRIBUTES + L_CONSTITUION;
+    public static final String K_INTELLIGENCE = H_ATTRIBUTES + L_INTELLIGENCE;
+    public static final String K_WISDOM = H_ATTRIBUTES + L_WISDOM;
+    public static final String K_CHARISMA = H_ATTRIBUTES + L_CHARISMA;
 
+    public static final String EQUIPPED = "is_equipped";
+    public static final String NOT_EQUIPPED = "not_equipped";
+    public static final String SET_TO_UNEQUIP = "setToUnequip";
+    public static final String UNEQUIPPED = "unequipped";
+    public static final String SET_TO_EQUIPPED = "setToEquip";
+
+    public static final String L_DESCRIPTION = "desc";
+    public static final String L_AC = "ac";
+    public static final String L_DEX_BONUS = "dexBonus";
+    public static final String L_EQUIP_STATE = "isEquipped?";
+
+    public static final String K_ARMOR_NAME = H_ARMOR + L_NAME;
+    public static final String K_ARMOR_DESCRIPTION = H_ARMOR + L_DESCRIPTION;
+    public static final String K_ARMOR_AC = H_ARMOR + L_AC;
+    public static final String K_ARMOR_DEX_BONUS = H_ARMOR + L_DEX_BONUS;
+    public static final String K_ARMOR_EQUIP_STATE = H_ARMOR + L_EQUIP_STATE;
+
+    public static final String L_ATTK_BONUS = "attkBonus";
+    public static final String L_DMG_ROLL = "dmgRoll";
+    public static final String L_DMG_BONUS = "dmgBonus";
+
+    public static final String K_WEAPON_ATTK_BONUS = H_WEAPON + L_ATTK_BONUS;
+    public static final String K_WEAPON_DMG_ROLL = H_WEAPON + L_DMG_ROLL;
+    public static final String K_WEAPON_DMG_BONUS = H_WEAPON + L_DMG_BONUS;
+    public static final String K_WEAPON_EQUIP_STATE = H_WEAPON + L_EQUIP_STATE;
+    public static final String K_WEAPON_NAME = H_WEAPON + L_NAME;
+    public static final String K_WEAPON_DESCRIPTION = H_WEAPON + L_DESCRIPTION;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Inventory low_keys
     //Inventory is a different way of handling keys because items have an index
     //This requires the usage of the [-optional_key] to derive extra information
@@ -51,16 +104,16 @@ public class KEY {
     //Key: "inventory*rowInde x(3)*item_desc"
     public static final String ITEM_NAME = "*item_name";
     public static final String ITEM_DESC = "*item_desc";
-    public static final String EQUIPPED = "*equipped";
+
     public static int invRow = 0;
     /**
-     *  Returns a high_key INVENTORY with a low_key that represents the element
+     *  Returns a high_key H_INVENTORY with a low_key that represents the element
      *  add an optional_key (see above) to describe what specific changes have
      *  to be made.
      */
     public static String generateInventoryRow(){
         invRow++;
-        return INVENTORY+"rowIndex("+0+")";
+        return H_INVENTORY +"rowIndex("+0+")";
     }
 
     //Inventory low_keys
@@ -71,20 +124,9 @@ public class KEY {
     public static final String SPELL_NAME = "*spell_name";
     public static final String SPELL_DESC = "*spell_desc";
     public static final String SPELL_LEVEL = "*spell_level";
-    public static int spellbookRow = 0;
-    /**
-     *  Returns an autoIncremented id to place for a inventory item.
-     */
-    public static String generateSpellbookRow(){
-        invRow++;
-        return INVENTORY+"rowIndex("+0+")";
-    }
 
 
-    public static final String EQUIPPED_WEAPON_NAME = EQUIPPED_WEAPON + "name";
-    public static final String EQUIPPED_WEAPON_DESC = EQUIPPED_WEAPON + "desc";
-
-
+    //Features are very much different from all the other ones as there are several Feature types.
 
 
 }
